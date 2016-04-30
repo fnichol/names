d9e89ad782a5e84d88b35a425dbd9dda0d36c4f7
Fletcher Nichol <fnichol@nichol.ca>
2016-04-29 19:39:55 -0600

    [cli] Use `clap_app!` macro and update version of clap.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,28 +2,26 @@
 extern crate clap;
 extern crate names;
 
-use clap::{App, Arg};
 use names::{Generator, Name};
 
 fn main() {
-    let matches = App::new("names")
-                      .version(&crate_version!()[..])
-                      .author("\nAuthor: Fletcher Nichol <fnichol@nichol.ca>\n")
-                      .about("A random name generator with results like `delirious-pail'.")
-                      .arg(Arg::with_name("amount")
-                               .help("Number of names to generate (default: 1)")
-                               .index(1))
-                      .arg(Arg::with_name("number")
-                               .short("n")
-                               .long("number")
-                               .help("Adds a random number to the name(s)"))
-                      .get_matches();
-
-    let amount = value_t!(matches.value_of("amount"), usize).unwrap_or(1);
-    let naming: Name = if matches.is_present("number") {
-        Name::Numbered
-    } else {
-        Default::default()
+    let (naming, amount) = {
+        let app = clap_app!(names =>
+            (version: &crate_version!()[..])
+            (author: "\nAuthor: Fletcher Nichol <fnichol@nichol.ca>\n")
+            (about: "A random name generator with results like `delirious-pail'.")
+            (@setting ColoredHelp)
+            (@arg amount: "Number of names to generate (default: 1)")
+            (@arg number: -n --number "Adds a random number to the name(s)")
+        );
+        let matches = app.get_matches();
+        let amount = value_t!(matches.value_of("amount"), usize).unwrap_or(1);
+        let naming: Name = if matches.is_present("number") {
+            Name::Numbered
+        } else {
+            Default::default()
+        };
+        (naming, amount)
     };
 
     let mut generator = Generator::with_naming(naming);
