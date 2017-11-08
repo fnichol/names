8f4b65acd9ec5849816edaa29ce8993a03864b15
Fletcher Nichol <fnichol@nichol.ca>
2017-11-08 13:17:35 -0700

    [lint] Update due to Rustfmt 0.9.0.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,13 +6,14 @@ use names::{Generator, Name};
 
 fn main() {
     let (naming, amount) = {
-        let app = clap_app!(names =>
-            (version: &crate_version!()[..])
-            (author: "\nAuthor: Fletcher Nichol <fnichol@nichol.ca>\n")
-            (about: "A random name generator with results like `delirious-pail'.")
-            (@setting ColoredHelp)
-            (@arg amount: "Number of names to generate (default: 1)")
-            (@arg number: -n --number "Adds a random number to the name(s)")
+        let app =
+            clap_app!(names =>
+                (version: &crate_version!()[..])
+                (author: "\nAuthor: Fletcher Nichol <fnichol@nichol.ca>\n")
+                (about: "A random name generator with results like `delirious-pail'.")
+                (@setting ColoredHelp)
+                (@arg amount: "Number of names to generate (default: 1)")
+                (@arg number: -n --number "Adds a random number to the name(s)")
         );
         let matches = app.get_matches();
         let amount = value_t!(matches.value_of("amount"), usize).unwrap_or(1);
